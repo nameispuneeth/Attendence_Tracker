2249d0401986c9fdfaa0a5b91cc9ab065f4ea97b
nameispuneeth <puneethkumarkaramala@gmail.com>
2025-09-02 01:45:06 +0530

    Draft 1

---
+++ client/src/Components/TimeTable.js
@@ -0,0 +1,49 @@
+import { useState } from "react"
+export default function TimeTable(){
+  const [File,setFile]=useState(null);
+  const HandleSubmit=async(e)=>{
+    e.preventDefault();
+    if(!File) return;
+    const formData = new FormData();
+    formData.append("file", File);
+
+    const response=await fetch("http://localhost:8000/api/getData",{
+      method:"POST",
+      body:formData
+    });
+
+    const data=await response.json();
+
+    console.log(data.response);
+
+  }
+  return(
+     <div className="h-screen bg-gray-700 flex justify-center items-center">
+      <form
+        onSubmit={HandleSubmit}
+        className="flex flex-col space-y-6 bg-white p-6 rounded-2xl shadow-md"
+      >
+        <input
+          type="file"
+          accept="image/*,.pdf,.xlsx"
+          onChange={(e) => setFile(e.target.files[0])}
+          className="block w-full text-sm text-gray-700 
+                     file:mr-4 file:py-2 file:px-4
+                     file:rounded-lg file:border-0
+                     file:text-sm file:font-semibold
+                     file:bg-blue-600 file:text-white
+                     hover:file:bg-blue-700 cursor-pointer"
+        />
+
+        <button
+          type="submit"
+          className="w-full py-2 px-4 bg-green-600 text-white 
+                     rounded-lg font-medium shadow-md
+                     hover:bg-green-700 transition duration-200"
+        >
+          Upload & Analyze
+        </button>
+      </form>
+    </div>
+  )
+}
--- server/index.js
+++ server/index.js
@@ -1,8 +1,75 @@
-const express=require("express");
-const app=express();
+require("dotenv").config();
 
-app.get("/",(req,res)=>{
-    res.send("Hello");
-})
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const cors = require("cors");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const app = express();
 
-app.listen(8000)
+app.use(cors())
+app.use(express.json())
+const upload = multer({ dest: "uploads/" });
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+
+const allowedMimeTypes = [
+    "image/png",
+    "image/jpeg",
+    "image/webp",
+    "application/pdf",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+];
+
+app.post("/api/getData", upload.single("file"), async (req, res) => {
+    try {
+        const filePath = req.file.path;
+        const fileMime = req.file.mimetype;
+
+        if (!allowedMimeTypes.includes(fileMime)) {
+            fs.unlinkSync(filePath);
+            res.send({ status: 'error', error: 'Invalid FileType' });
+        }
+
+        const fileBuffer = fs.readFileSync(filePath);
+        const base64 = fileBuffer.toString("base64");
+
+        const result = await model.generateContent([
+            {
+                inlineData: {
+                    mimeType: fileMime,
+                    data: base64
+                },
+            },
+
+            { text: "If the Sent file is a time table generate a js object of it with days as keys and each hour represent by index from 0 to 7,else return an empty object and Return ONLY a valid JSON object. Do not include explanations or text.." },
+
+        ]);
+
+        fs.unlinkSync(filePath);
+        let rawText = result.response.text();
+
+        rawText = rawText.replace(/```json|```/g, "").trim();
+
+        let jsonResponse;
+        try {
+            jsonResponse = JSON.parse(rawText);
+        } catch (err) {
+            console.error("❌ Failed to parse JSON:", rawText);
+            jsonResponse = {};
+        }
+
+        res.json({ response: jsonResponse });
+    } catch {
+        res.send({ status: 'error', error: 'Network Issues' })
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("BradPitt");
+});
+
+app.listen(8000, () => {
+    console.log("✅ PORT IS RUNNING AT http://localhost:8000");
+});
